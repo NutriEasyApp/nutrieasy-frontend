b36b2de2c0c44d860c8bbe88014f7c6fbd2aa076
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-03-13 20:35:29 -0300

    change component login

--- app/components/Login.js
+++ app/components/Login.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import {
   StyleSheet,
@@ -7,84 +6,19 @@ import {
   KeyboardAvoidingView,
   Image,
   TextInput,
-  TouchableOpacity,
-  Animated,
-  Keyboard
+  TouchableOpacity
 } from 'react-native';
 
-export default function Login() {
 
-  const [offset] = useState(new Animated.ValueXY({ x: 0, y: 80 }));
-  const [opacity] = useState(new Animated.Value(0));
-  const [logo] = useState(new Animated.ValueXY({
-    x: 130,
-    y: 155
-  }));
-
-  useEffect(() => {
-    keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', keyboardDidShow);
-    keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', keyboardDidHide);
-
-    Animated.parallel([
-      Animated.spring(offset.y, {
-        toValue: 0,
-        speed: 4,
-        bounciness: 30
-      }),
-      Animated.timing(opacity, {
-        toValue: 1,
-        duration: 200,
-      })
-    ]).start();
-  }, []);
-
-  function keyboardDidShow() {
-    Animated.parallel([
-      Animated.timing(logo.x, {
-        toValue: 55,
-        duration: 100,
-      }),
-      Animated.timing(logo.y, {
-        toValue: 65,
-        duration: 100,
-      }),
-    ]).start();
-  }
-
-  function keyboardDidHide() {
-    Animated.parallel([
-      Animated.timing(logo.x, {
-        toValue: 130,
-        duration: 100,
-      }),
-      Animated.timing(logo.y, {
-        toValue: 155,
-        duration: 100,
-      }),
-    ]).start();
-  }
+export default function App() {
 
   return (
     <KeyboardAvoidingView style={styles.background}>
       <View style={styles.containerLogo}>
-        <Animated.Image
-          style={{
-            width: logo.x,
-            height: logo.y
-          }}
-          source={require('./assets/images/logo.png')}>
-
-        </Animated.Image>
-        <StatusBar style="auto" />
+        <Image style={styles.logoImg} source={require('./assets/images/logo.png')}></Image>
       </View>
-      <Animated.View style={[styles.container, {
-        opacity: opacity,
-        transform: [
-          {
-            translateY: offset.y
-          }
-        ]
-      }]}>
+
+      <View style={styles.container}>
         <TextInput style={styles.input} placeholder="Email" autoCorrect={false} onChangeText={() => { }}></TextInput>
         <TextInput style={styles.input} placeholder="Senha" autoCorrect={false} onChangeText={() => { }}></TextInput>
         <TouchableOpacity style={styles.btnSubmit}>
@@ -97,7 +31,7 @@ export default function Login() {
             Criar Conta
           </Text>
         </TouchableOpacity>
-      </Animated.View>
+      </View>
     </KeyboardAvoidingView>
   );
 }
@@ -107,13 +41,16 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    //backgroundColor: '#191919'
     backgroundColor: '#fff'
   },
   containerLogo: {
     flex: 1,
     justifyContent: 'center',
     marginBottom: -350
+  },
+  logoImg: {
+    width: 180,
+    height: 180
   },
   container: {
     flex: 1,
