f2e56fbcf5a4c3c97f302c64c70a954afc75d8a2
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-03-16 07:15:02 -0300

    correct conflict

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
 import * as React from 'react';
 import { View } from 'react-native';
 import Routes from  './src/app/routes';
-<<<<<<< HEAD
-=======
-
->>>>>>> development
 
 export default function App() {
   return (
