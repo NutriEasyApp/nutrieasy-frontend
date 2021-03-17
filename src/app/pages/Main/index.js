2bd95dcbe4fa5709271a4f035dbf0f16ac2d0161
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-03-17 20:19:19 -0300

    add login index main

--- src/app/pages/Main/index.js
+++ src/app/pages/Main/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import { Container } from './styles';
 import Login  from '../../components/Login/Login';
 
-
-import HealthAnalysis  from '../../components/HealthAnalysis/FormHealthAnalysis';
-
 export default function Main() {
     return (
-        <HealthAnalysis />
+        <Login />
     );
 }
