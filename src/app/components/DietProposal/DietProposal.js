b8cf228c38cb8c600c7000bd645ee7ba1b4b316e
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-03-21 16:34:56 -0300

    :hammer: update diet proposal

--- src/app/components/DietProposal/DietProposal.js
+++ src/app/components/DietProposal/DietProposal.js
@@ -16,34 +16,40 @@ export default function DietProposal() {
 
     return (
         <View style={styles.background}>
+
             <Text style={{fontSize: 27, fontWeight: '700', marginTop: 30, marginBottom: 15}}>Proposta de Dieta</Text>
+
             <DataTable>
                 <DataTable.Header>
-                    <DataTable.Title>Dessert</DataTable.Title>
-                    <DataTable.Title numeric>Calories</DataTable.Title>
-                    <DataTable.Title numeric>Fat</DataTable.Title>
+                    <DataTable.Title>Nº Dieta</DataTable.Title>
+                    <DataTable.Title numeric>Carboidratos</DataTable.Title>
+                    <DataTable.Title numeric>Proteínas</DataTable.Title>
+                    <DataTable.Title numeric>Lipídeos</DataTable.Title>
                 </DataTable.Header>
 
                 <DataTable.Row>
-                    <DataTable.Cell>Frozen yogurt</DataTable.Cell>
+                    <DataTable.Cell>1</DataTable.Cell>
+                    <DataTable.Cell numeric>159</DataTable.Cell>
                     <DataTable.Cell numeric>159</DataTable.Cell>
                     <DataTable.Cell numeric>6.0</DataTable.Cell>
                 </DataTable.Row>
 
                 <DataTable.Row>
-                    <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
+                    <DataTable.Cell>2</DataTable.Cell>
+                    <DataTable.Cell numeric>159</DataTable.Cell>
                     <DataTable.Cell numeric>237</DataTable.Cell>
                     <DataTable.Cell numeric>8.0</DataTable.Cell>
                 </DataTable.Row>
 
-                <DataTable.Pagination
+                {/*<DataTable.Pagination
                     page={1}
                     numberOfPages={3}
                     onPageChange={page => {
                         console.log(page);
                     }}
                     label="1-2 of 6"
-                />
+                />*/}
+
             </DataTable>
         </View>
     );
--- src/app/components/DietProposal/DietProposal.style.js
+++ src/app/components/DietProposal/DietProposal.style.js
@@ -6,49 +6,12 @@ export const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#fff'
-    },    
-    containerLogo: {
-        justifyContent: 'center',
-        marginBottom: -20,
-        marginTop: 0
-    },
-    logoImg: {
-        width: 220,
-        height: 220
-    },    
+    },   
     container: {
         alignItems: 'center',
         justifyContent: 'center',
-        width: '90%',
-        paddingBottom: 50,
+        width: '100%',
+        paddingBottom: 0,
         marginTop: 0,
-    },    
-    input: {
-        backgroundColor: '#f9f9f9',
-        width: '90%',
-        marginBottom: 15,
-        color: '#222',
-        fontSize: 16,
-        borderRadius: 7,
-        padding: 15,
-        height: 55
-    },    
-    btnSubmit: {
-        backgroundColor: '#90cc0c',
-        width: '90%',
-        height: 45,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 7,
-    },
-    submitText: {
-        color: "#FFF",
-        fontSize: 18
-    },
-    btnRegister: {
-        marginTop: 10
-    },
-    registerText: {
-        color: '#3b3b3b',
     }
 });
