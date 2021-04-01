2cb7fee6092a25f800b5f29a94e8fe92aec09ef0
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-03-31 22:29:17 -0300

    :hammer: update the page development scripts

--- src/app/pages/DietProposal/index.js
+++ src/app/pages/DietProposal/index.js
@@ -1,6 +1,62 @@
 import React from 'react';
-import DietProposal from '../../components/DietProposal/DietProposal';
+import { styles } from './style';
+import { DataTable } from 'react-native-paper';
 
-export default function DietProposalIndex() {
-  return <DietProposal />;
+import {
+  Text,
+  View,
+  KeyboardAvoidingView,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+
+export default function DietProposal() {
+  return (
+    <View style={styles.background}>
+      <Text
+        style={{
+          fontSize: 27,
+          fontWeight: '700',
+          marginTop: 30,
+          marginBottom: 15,
+        }}
+      >
+        Proposta de Dieta
+      </Text>
+
+      <DataTable>
+        <DataTable.Header>
+          <DataTable.Title>Nº Dieta</DataTable.Title>
+          <DataTable.Title numeric>Carboidratos</DataTable.Title>
+          <DataTable.Title numeric>Proteínas</DataTable.Title>
+          <DataTable.Title numeric>Lipídeos</DataTable.Title>
+        </DataTable.Header>
+
+        <DataTable.Row>
+          <DataTable.Cell>1</DataTable.Cell>
+          <DataTable.Cell numeric>159</DataTable.Cell>
+          <DataTable.Cell numeric>159</DataTable.Cell>
+          <DataTable.Cell numeric>6.0</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          <DataTable.Cell>2</DataTable.Cell>
+          <DataTable.Cell numeric>159</DataTable.Cell>
+          <DataTable.Cell numeric>237</DataTable.Cell>
+          <DataTable.Cell numeric>8.0</DataTable.Cell>
+        </DataTable.Row>
+
+        {/*<DataTable.Pagination
+          page={1}
+          numberOfPages={3}
+          onPageChange={page => {
+          console.log(page);
+          }}
+          label="1-2 of 6"
+        />*/}
+        
+      </DataTable>
+    </View>
+  );
 }
--- src/app/pages/SignUp/index.js
+++ src/app/pages/SignUp/index.js
@@ -1,7 +1,53 @@
 import React from 'react';
-import { View } from 'react-native';
-import Signup from '../../components/Signup/Signup';
+import { styles } from './style';
 
-export default function SignupIndex() {
-  return <Signup />;
+import {
+  Text,
+  View,
+  KeyboardAvoidingView,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+
+const logoImg = require('../../../assets/images/logo.png');
+
+export default function Signup() {
+  return (
+    <KeyboardAvoidingView style={styles.background}>
+
+      <View style={styles.containerLogo}>
+        <Image style={styles.logoImg} source={logoImg} />
+      </View>
+
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome completo"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="E-mail"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+          placeholder="Senha"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+
+        <TouchableOpacity style={styles.btnSubmit}>
+          <Text style={styles.submitText}>Cadastrar conta</Text>
+        </TouchableOpacity>
+        
+      </View>
+    </KeyboardAvoidingView>
+  );
 }
