f422d052cfa395f203f20de85f711d3208c3dfdc
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-03-31 22:34:27 -0300

    :hammer: update services api

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.15.121:3080',
+  /* baseURL: 'http://192.168.15.121:3080', */
+  baseURL: 'http://192.168.0.5:19000',
 });
 
 export default api;
