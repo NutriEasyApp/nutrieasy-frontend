44dcd315549c7d9422248a91817b416f3180e608
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-03-31 22:33:40 -0300

    :hammer: update app.js and services api

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
-import * as React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { AuthProvider } from './src/app/contexts/auth';
 import Routes from './src/app/routes';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <NavigationContainer>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </NavigationContainer>
+  );
 }
