203f9f9809c1b608121b0784943a607458032108
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-03-31 22:23:55 -0300

    :wastebasket: delete pages deprecate

--- src/app/pages/FormHealthAnalysis/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import FormHealthAnalysis from '../../components/HealthAnalysis/FormHealthAnalysis';
-
-export default function FormHealthAnalysisIndex() {
-  return <FormHealthAnalysis />;
-}
--- src/app/pages/Main/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Container } from './styles';
-import Menu from '../../components/Menu/Menu';
-
-export default function MainIndex() {
-  return <Menu />;
-}
