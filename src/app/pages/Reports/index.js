6258042ebf46bfe973eb5a634527935684112104
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-05-04 20:45:24 -0300

    :sparkles: add feat reports

--- src/app/pages/Reports/index.js
+++ src/app/pages/Reports/index.js
@@ -1,45 +1,128 @@
 import React from 'react';
-import { styles } from './style';
-
+import { Divisor, Content, Info } from './style';
 import {
-  Text,
-  View,
-  KeyboardAvoidingView,
-  Image,
-  TextInput,
-  TouchableOpacity,
-} from 'react-native';
+  PieChart,
+  Grid,
+  LineChart,
+  XAxis,
+  YAxis,
+} from 'react-native-svg-charts';
+import { Text } from 'react-native-svg';
+import { Title } from 'react-native-paper';
+export default function ReportsIndex() {
+  /*BEGIN Grafico 1 */
+  const data2 = [
+    50,
+    10,
+    40,
+    95,
+    -4,
+    -24,
+    85,
+    91,
+    35,
+    53,
+    -53,
+    24,
+    50,
+    -20,
+    -80,
+  ];
+  const axesSvg = { fontSize: 10, fill: 'grey' };
+  const verticalContentInset = { top: 10, bottom: 10 };
+  const xAxisHeight = 30;
 
-export default function Reports() {
+  const Labels = ({ slices, height, width }) => {
+    return slices.map((slice, index) => {
+      const { labelCentroid, pieCentroid, data } = slice;
+      return (
+        <Text
+          key={index}
+          x={pieCentroid[0]}
+          y={pieCentroid[1]}
+          fill={'white'}
+          textAnchor={'middle'}
+          alignmentBaseline={'middle'}
+          fontSize={24}
+          stroke={'black'}
+          strokeWidth={0.2}
+        >
+          {data.amount}
+        </Text>
+      );
+    });
+  };
+  /*END  Grafico 1 */
+
+  /* BEGIN Grafico 2 */
+  const data = [
+    {
+      key: 1,
+      amount: 50,
+      svg: { fill: '#C97600' },
+    },
+    {
+      key: 2,
+      amount: 50,
+      svg: { fill: '#FC9E19' },
+    },
+    {
+      key: 3,
+      amount: 40,
+      svg: { fill: '#FC9401' },
+    },
+    {
+      key: 4,
+      amount: 95,
+      svg: { fill: '#FDB34C' },
+    },
+    {
+      key: 5,
+      amount: 35,
+      svg: { fill: '#E38400' },
+    },
+  ];
+  /*END  Grafico 2 */
+
   return (
-    <KeyboardAvoidingView style={styles.background}>
-      <View style={styles.container}>
-        <TextInput
-          style={styles.input}
-          placeholder="Nome completo"
-          autoCorrect={false}
-          onChangeText={() => {}}
+    <Divisor>
+      <Title style={{ textAlign: 'center', fontSize: 25, fontWeight: 'bold' }}>
+        Relatórios
+      </Title>
+      <Content>
+        <YAxis
+          data={data2}
+          style={{ marginBottom: xAxisHeight }}
+          contentInset={verticalContentInset}
+          svg={axesSvg}
         />
-
-        <TextInput
-          style={styles.input}
-          placeholder="E-mail"
-          autoCorrect={false}
-          onChangeText={() => {}}
-        />
-
-        <TextInput
-          style={styles.input}
-          secureTextEntry={true}
-          placeholder="Senha"
-          autoCorrect={false}
-          onChangeText={() => {}}
-        />
-
-        <TouchableOpacity style={styles.btnSubmit}>
-          <Text style={styles.submitText}>Cadastrar conta</Text>
-        </TouchableOpacity>
-      </View>
-    </KeyboardAvoidingView>
+        <Info>
+          <LineChart
+            style={{ flex: 1 }}
+            data={data2}
+            contentInset={verticalContentInset}
+            svg={{ stroke: '#FC9401' }}
+          >
+            <Grid />
+          </LineChart>
+          <XAxis
+            style={{ marginHorizontal: -10, height: xAxisHeight }}
+            data={data2}
+            formatLabel={(value, index) => index}
+            contentInset={{ left: 10, right: 10 }}
+            svg={axesSvg}
+          />
+        </Info>
+      </Content>
+      <PieChart
+        style={{ height: 200 }}
+        valueAccessor={({ item }) => item.amount}
+        data={data}
+        spacing={0}
+        outerRadius={'95%'}
+      >
+        <Labels />
+      </PieChart>
+    </Divisor>
   );
 }
--- src/app/pages/Reports/style.js
+++ src/app/pages/Reports/style.js
@@ -1,54 +1,16 @@
-import { StyleSheet } from 'react-native';
+import styled from 'styled-components/native';
 
-export const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
-  },
-  containerLogo: {
-    justifyContent: 'center',
-    marginBottom: -20,
-    marginTop: 0,
-  },
-  logoImg: {
-    width: 220,
-    height: 220,
-  },
-  container: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '90%',
-    paddingBottom: 50,
-    marginTop: 0,
-  },
-  input: {
-    backgroundColor: '#f9f9f9',
-    width: '90%',
-    marginBottom: 15,
-    color: '#222',
-    fontSize: 16,
-    borderRadius: 7,
-    padding: 15,
-    height: 55,
-  },
-  btnSubmit: {
-    backgroundColor: '#90cc0c',
-    width: '90%',
-    height: 45,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 7,
-  },
-  submitText: {
-    color: '#FFF',
-    fontSize: 18,
-  },
-  btnRegister: {
-    marginTop: 10,
-  },
-  registerText: {
-    color: '#3b3b3b',
-  },
-});
+export const Divisor = styled.View`
+  margin-top: 100px;
+`;
+
+export const Content = styled.View`
+  height: 200px;
+  padding: 20px;
+  flex-direction: row;
+`;
+
+export const Info = styled.View`
+  flex: 1;
+  margin-left: 10px;
+`;
