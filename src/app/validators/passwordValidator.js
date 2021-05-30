fb79427b88d83eaf47c60c1b75792696361a3f9a
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-05-30 09:40:53 -0300

    :wastebasket: remove folder validators

--- src/app/validators/passwordValidator.js
+++
@@ -1,6 +0,0 @@
-export function passwordValidator(){
-    if(!password) return "A senha não pode estar em branco."
-    if(password.length < 5) return "A senha deve ter pelo menos 5 caracteres."
-
-    return ''
-}
