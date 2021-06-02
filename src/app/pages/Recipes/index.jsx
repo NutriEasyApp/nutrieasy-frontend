3a9c4514e0627cc7b36b89291ca49de081fecca0
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-06-02 19:17:12 -0300

    :hammer: update recipes

--- src/app/pages/Recipes/index.jsx
+++ src/app/pages/Recipes/index.jsx
@@ -3,19 +3,7 @@ import { View, ScrollView, SafeAreaView } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import {
-  Container,
-  Content,
-  Info,
-  TitleMain,
-  Icon,
-  InfoText,
-  Wrapper,
-  Value,
-  TitleElement,
-  TextMain,
-  SafeAreaViewStyle,
-} from "./style";
+import { Container, Content, Info, TitleMain, Icon, InfoText, Wrapper, Value, TitleElement, TextMain, SafeAreaViewStyle } from "./style";
 
 import api from "../../services/api";
 import AuthContext from "../../contexts/auth";
@@ -54,7 +42,6 @@ export default function Recipes() {
       } catch (err) {
         const { status } = err.response;
         if (status === 404) {
-          //console.log('Parece que você ainda não cadastrou sua ficha de saude');
           setDiet(false);
         }
         if (status === 401) {
@@ -70,67 +57,80 @@ export default function Recipes() {
       <ScrollView>
         <Container>
           <TitleMain>Divisão de Refeições</TitleMain>
-          <TextMain>
-            O objetivo dessa divisão é fornecer números adequados dos nutrientes para cada montante
-            de refeição, seja ela durante o dia ou um total por refeições.
-          </TextMain>
+          <TextMain>O objetivo dessa divisão é fornecer números adequados dos nutrientes para cada montante de refeição, seja ela durante o dia ou um total por refeições.</TextMain>
 
           <Wrapper>
-            <Icon>
-              <MaterialCommunityIcons name="calendar-today" color={"#90cc0c"} size={65} />
-            </Icon>
+            {diet ? (
+              <>
+                <Icon>
+                  <MaterialCommunityIcons name="calendar-today" color={"#90cc0c"} size={65} />
+                </Icon>
 
-            <TitleElement>Montante Nutricional por dia:</TitleElement>
+                <TitleElement>Montante Nutricional por dia:</TitleElement>
 
-            <Content>
-              <View>
-                <Info>
-                  <InfoText>
-                    Água: <Value>{proposed.diet.diet.water}</Value>
-                  </InfoText>
-                  <InfoText>
-                    Proteína: <Value>{proposed.diet.diet.protein}</Value>
-                  </InfoText>
-                  <InfoText>
-                    Caboidratos: <Value>{proposed.diet.diet.carbohydrates}</Value>
-                  </InfoText>
-                  <InfoText>
-                    Lípidios: <Value>{proposed.diet.diet.lipids}</Value>
-                  </InfoText>
-                  <InfoText>
-                    Calorias: <Value>{proposed.diet.diet.calories}</Value>
-                  </InfoText>
-                </Info>
-              </View>
-            </Content>
+                <Content>
+                  <View>
+                    <Info>
+                      <InfoText>
+                        Água: <Value>{proposed.diet.diet.water}</Value>
+                      </InfoText>
+                      <InfoText>
+                        Proteína: <Value>{proposed.diet.diet.protein}</Value>
+                      </InfoText>
+                      <InfoText>
+                        Caboidratos: <Value>{proposed.diet.diet.carbohydrates}</Value>
+                      </InfoText>
+                      <InfoText>
+                        Lípidios: <Value>{proposed.diet.diet.lipids}</Value>
+                      </InfoText>
+                      <InfoText>
+                        Calorias: <Value>{proposed.diet.diet.calories}</Value>
+                      </InfoText>
+                    </Info>
+                  </View>
+                </Content>
+              </>
+            ) : (
+              <>
+                <Value>Parece que você ainda não cadastrou sua ficha de saúde.</Value>
+              </>
+            )}
           </Wrapper>
 
           <Wrapper style={{ marginTop: 40 }}>
-            <Icon>
-              <MaterialCommunityIcons name="noodles" color={"#90cc0c"} size={65} />
-            </Icon>
-            <TitleElement>Montante Nutricional por refeições:</TitleElement>
-            <Content>
-              <View>
-                <Info>
-                  <InfoText>
-                    Água: <Value>{proposed.diet.dietPerMeal.water}</Value>
-                  </InfoText>
-                  <InfoText>
-                    Proteína: <Value>{proposed.diet.dietPerMeal.protein}</Value>
-                  </InfoText>
-                  <InfoText>
-                    Caboidratos: <Value>{proposed.diet.dietPerMeal.carbohydrates}</Value>
-                  </InfoText>
-                  <InfoText>
-                    Lípidios: <Value>{proposed.diet.dietPerMeal.lipids}</Value>
-                  </InfoText>
-                  <InfoText>
-                    Calorias: <Value>{proposed.diet.dietPerMeal.calories}</Value>
-                  </InfoText>
-                </Info>
-              </View>
-            </Content>
+            {diet ? (
+              <>
+                <Icon>
+                  <MaterialCommunityIcons name="noodles" color={"#90cc0c"} size={65} />
+                </Icon>
+                <TitleElement>Montante Nutricional por refeições:</TitleElement>
+                <Content>
+                  <View>
+                    <Info>
+                      <InfoText>
+                        Água: <Value>{proposed.diet.dietPerMeal.water}</Value>
+                      </InfoText>
+                      <InfoText>
+                        Proteína: <Value>{proposed.diet.dietPerMeal.protein}</Value>
+                      </InfoText>
+                      <InfoText>
+                        Caboidratos: <Value>{proposed.diet.dietPerMeal.carbohydrates}</Value>
+                      </InfoText>
+                      <InfoText>
+                        Lípidios: <Value>{proposed.diet.dietPerMeal.lipids}</Value>
+                      </InfoText>
+                      <InfoText>
+                        Calorias: <Value>{proposed.diet.dietPerMeal.calories}</Value>
+                      </InfoText>
+                    </Info>
+                  </View>
+                </Content>
+              </>
+            ) : (
+              <>
+                <Value>Parece que você ainda não cadastrou sua ficha de saúde.</Value>
+              </>
+            )}
           </Wrapper>
         </Container>
       </ScrollView>
